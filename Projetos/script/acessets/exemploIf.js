e185d8e734d085658f00595f24d63af5f34af0ab
Liliane9235 <lilianegatalinda19@gmail.com>
2026-04-22 20:08:21 -0400

    primeira versão

---
+++ Projetos/script/acessets/exemploIf.js
@@ -0,0 +1,26 @@
+let idade = prompt ("Quantos anos você tem?");
+if (idade < 18 ) {
+    alert ("você é menor de idade.");
+} else if ( idade >= 18 && idade <= 65){
+    alert ( "você é adulto.");
+} else {
+alert ( "você é idoso ");
+
+
+
+
+
+
+
+
+ 
+
+// let numero = "20";
+ // let numero2 = 20;
+ // if (numero == numero2){
+ // alert("Os números sao iguais.");
+ // let soma = (numero) + numero2;
+ // else {
+ 
+
+
---
+++ Projetos/script/acessets/script.js
@@ -0,0 +1,18 @@
+let nome = "Liliane";
+let idade =  35; 
+let altura = 1.58;
+let peso = 60.0
+let estudante = true;
+let hobbies = ["programação", "jogos","música"]
+    let endereço = {
+        rua: "Rua principal",
+        numero: 123,
+    }
+
+   console.log("nome:",nome);
+   console.log("idade:", idade);
+   console.log("altura:",altura);
+   console.log("peso:",peso);
+   console.log("É estudante:",estudante);
+   console.log("hobbies:", hobbies[0])
+   console.log("endereço:",endereço);
